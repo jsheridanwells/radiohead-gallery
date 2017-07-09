fe42c67da664fd6c550d9d0968d2d580a6bed7c9
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-07-08 20:01:20 -0500

    started js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var images = document.getElementsByTagName('img');
+var overlay = document.getElementById('overlay');
+
+function showOverlay() {
+	
+}
+
+//user clicks image
+
+//#overlay is displayed
+
+//overlay image gets src of clicked image
+
+//overlay p gets alt of clicked image
+
+
+
