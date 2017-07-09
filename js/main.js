7a0a03ba1c0716e48df98f036cb3ee53905ed1a1
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-07-09 15:12:45 -0500

    created first jquery functions for showing overlay when image is clicked

--- js/main.js
+++ js/main.js
@@ -1,26 +1,65 @@
-var images = document.getElementsByTagName('img');
-var overlay = document.getElementById('overlay');
+//STATUS: CANNOT GET SRC AND ALT ATTRITBUES ONCLICK
+//CANNOT GET SRC AND ALT ATTR OF SPECIFIC IMAGE CLICKED
+//REASON: 'THIS' IS WINDOW, NOT IMAGE. 
+//HOW CAN WE MAKE THIS REFER TO IMAGE??
 
+//select clickable elements
+var $images = $('section img');//thumbnail images
+var $overlay = $('#overlay');//overlay
+var $left = $('#left');//left arrow
+var $right = $('#right')//right arrow
+var $source = '';
+var $alt = '';
 
+//show overlay
 function showOverlay() {
-	overlay.className = 'show';
+	$($overlay).removeClass('hide');
+	$($overlay).addClass('show');
 }
 
+//hide overlay
 function hideOverlay() {
-	overlay.className = 'hide';
+	$($overlay).removeClass('show');
+	$($overlay).addClass('hide');
 }
 
-for (var i = 0; i < images.length; i++) {
-	images[i].onclick= showOverlay;
+//get img src of thumbnail
+function getSrc() {
+	var image = $(this);
+	console.log(image);
 }
 
-//user clicks image
+//get alt text of thumbnail
+function getAlt() {
+	$alt = $(this).attr('alt');
+	console.log($alt);
+}
+//when thumbnail is clicked
+$($images).click(function() {
+	getSrc();
+	getAlt();
+	showOverlay();
+});
 
-//#overlay is displayed
 
-//overlay image gets src of clicked image
+//when outside space on overlay is clicked
+$($overlay).click(hideOverlay);
 
-//overlay p gets alt of clicked image
+//or when esc key is pressed
+$(document).keyup(function(e){
+	if(e.keyCode === 27) {
+		hideOverlay();
+	}
+});
 
+
+
+//overlay img src is thumbnail src
+//overlay p text is thumbnail alt
+
+//get img filename from array
+
+//left arrow shows previous image in array
+//right arrow shows next image in array
 
 
