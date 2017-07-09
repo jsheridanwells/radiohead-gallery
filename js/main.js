b4f239d57d2208ee31124921833e3225f9d03342
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-07-09 07:59:19 -0500

    overlay shows when image clicked

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 var images = document.getElementsByTagName('img');
 var overlay = document.getElementById('overlay');
 
+
 function showOverlay() {
-	
+	overlay.className = 'show';
+}
+
+function hideOverlay() {
+	overlay.className = 'hide';
+}
+
+for (var i = 0; i < images.length; i++) {
+	images[i].onclick= showOverlay;
 }
 
 //user clicks image
